a239d1d4d4b346097ea6105601b1c56092cf512f
Ed <edmundocall.eoc@gmail.com>
2020-02-04 11:13:04 +0000

    fix missing argument and typos

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -15,7 +15,7 @@ function userInformationHTML(user){
         </div>`;
 }
 
-function repoInformationHTML(){
+function repoInformationHTML(repos){
     if(repos.length == 0){
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
@@ -54,30 +54,26 @@ function fetchGitHubInformation(event){
         </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     )
     .then(
-        function(response){
-
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
-
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
-        function(errResponse){
-
-            if(errResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-            }
-            else {
-
-                console.log(errResponse);
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errResponse.responseJSON.message}</h2>`);
-
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-
-        }
-    );
+    });
 }
 
 $(document).ready(fetchGitHubInformation);
