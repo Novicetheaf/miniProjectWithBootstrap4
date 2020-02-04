5578a3f152800b421bcc348a13501d6b8c63b4d8
Ed <edmundocall.eoc@gmail.com>
2020-02-04 11:00:55 +0000

    fix for bugs of empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,9 @@ function repoInformationHTML(){
 
 function fetchGitHubInformation(event){
 
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
 
     if(!username) {
@@ -76,3 +79,5 @@ function fetchGitHubInformation(event){
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
